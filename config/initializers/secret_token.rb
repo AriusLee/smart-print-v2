026926584c56253dfd019bba70072877e9231e2e
Arius <ariusleechihin@gmail.com>
2012-11-07 23:00:07 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartPrintV2::Application.config.secret_token = 'fd4f5f58974209a2a7c1ce87422b793d8e6cc44b1580730970a028136bfb23e0580d00cfce86bb5cfc19a981a2ed5d7df58a1305cb78fbfcd85e7f7934fe54ae'
