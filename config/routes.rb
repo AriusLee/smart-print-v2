a999a4b70ce77bdffe1f2907765f6b44b7f30f1a
Arius <ariusleechihin@gmail.com>
2012-11-22 07:55:46 +0800

    Product show.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ SmartPrintV2::Application.routes.draw do
 
   resources :products, :only => ['index']
 
+  resource :products, :path => 'products', :only => [] do
+     collection do
+       get 'popup_system'
+     end
+   end
+
 end
